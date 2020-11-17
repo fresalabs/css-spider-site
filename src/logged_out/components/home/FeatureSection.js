b91abbe16f322135f9c66a310847bdb9c6080b1f
Aswin Vayiravan <aswin@deducely.com>
2020-11-16 21:46:30 -0500

    added feature copy

--- src/logged_out/components/home/FeatureSection.js
+++ src/logged_out/components/home/FeatureSection.js
@@ -18,36 +18,36 @@ const iconSize = 30;
 const features = [
   {
     color: "#00C853",
-    headline: "Feature 1",
+    headline: "Better CSS Inspection",
     text:
-      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+      "Let us be honest, the browser console was not developed with usability in mind. The inspect element pane is cluttered. Just like you, we also love a great user experience, CSS spider gives you an uncluttered interface to view the CSS rules behind all elements present on webpages.",
     icon: <BuildIcon style={{ fontSize: iconSize }} />,
     mdDelay: "0",
     smDelay: "0"
   },
   {
     color: "#6200EA",
-    headline: "Feature 2",
+    headline: "Easy CSS editing",
     text:
-      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+      "Editing and keeping track of edited the CSS rules on the browser console can become a hassle, CSS spider makes that process simple, just hit the spacebar to edit the CSS rules of the surrent element.",
     icon: <CalendarTodayIcon style={{ fontSize: iconSize }} />,
     mdDelay: "200",
     smDelay: "200"
   },
   {
     color: "#0091EA",
-    headline: "Feature 3",
+    headline: "One-click CSS copy",
     text:
-      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+      "We might want to copy all the CSS rules on a particular DOM element for debugging, or we might stumbleupon a browser element that is well designed, CSS spider enables you to copy the CSS rules in a jiffy so that you can take it out and tinker further. ",
     icon: <MeassageIcon style={{ fontSize: iconSize }} />,
     mdDelay: "400",
     smDelay: "0"
   },
   {
     color: "#d50000",
-    headline: "Feature 4",
+    headline: "Send CSS to Codepen",
     text:
-      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+      "Codepen has become the instagram for Frontend devs, so we have built a one click share feature to instantaneously port out the CSS rules that you like to Codepen.  ",
     icon: <ComputerIcon style={{ fontSize: iconSize }} />,
     mdDelay: "0",
     smDelay: "200"
